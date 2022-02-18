afcb5e127ed9c56e4d7367788aee70458172bc35
Ketch Zeng <zengkeqi1999@163.com>
2022-02-18 22:36:17 +0800

    add stylua

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 # in .dotfiles directory
 ln -s $(pwd)/zsh/zshrc ~/.zshrc
 ln -s $(pwd)/starship/starship.toml ~/.config/starship.toml
+ln -s $(pwd)/stylua/stylua.toml ~/.config/stylua.toml
 ln -s $(pwd)/ranger ~/.config/ranger
 ln -s $(pwd)/kitty ~/.config/kitty
 
