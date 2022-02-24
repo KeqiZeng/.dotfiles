7cac538ac5fb2d536a67e149654d7116959e3d16
Ketch Zeng <zengkeqi1999@163.com>
2022-02-25 00:35:23 +0800

    add gitconfig

--- setup.sh
+++ setup.sh
@@ -5,5 +5,6 @@ ln -s $(pwd)/starship/starship.toml ~/.config/starship.toml
 ln -s $(pwd)/stylua/stylua.toml ~/.config/stylua.toml
 ln -s $(pwd)/ranger ~/.config/ranger
 ln -s $(pwd)/kitty ~/.config/kitty
+ln -s $(pwd)/gitconfig/.gitconfig ~/.gitconfig
 
 # brew install fzf ranger starship thefuck zoxide bat
