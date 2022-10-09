944af22d3017fc185b05757d8519eede5d9890eb
Ketch Zeng <zengkeqi1999@163.com>
2022-10-09 17:14:17 +0200

    add emoji in neofetch

--- setup.sh
+++ setup.sh
@@ -4,10 +4,11 @@ ln -s $(pwd)/zsh/zshrc ~/.zshrc
 ln -s $(pwd)/starship/starship.toml ~/.config/starship.toml
 ln -s $(pwd)/stylua/stylua.toml ~/.config/stylua.toml
 ln -s $(pwd)/ranger ~/.config/ranger
+ln -s $(pwd)/neofetch ~/.config/neofetch
 ln -s $(pwd)/kitty ~/.config/kitty
 ln -s $(pwd)/gitconfig/.gitconfig ~/.gitconfig
 ln -s $(pwd)/tealdeer ~/.config/tealdeer
 ln -s $(pwd)/tokei/.tokeirc ~/.tokeirc
 ln -s $(pwd)/latexmk/.latexmkrc ~/.latexmkrc
 
-# brew install fzf ranger starship thefuck zoxide bat tokei tealdeer
+# brew install fzf fd ripgrep ranger starship thefuck zoxide bat exa tokei tealdeer
